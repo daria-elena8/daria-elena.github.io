ceccd2120a651d879714dd09a50b4c04198a430f
daria-elena8 <116504707+daria-elena8@users.noreply.github.com>
2023-05-25 02:16:39 +0300

    added functions and comment section

--- FirstPage.js
+++ FirstPage.js
@@ -6,3 +6,122 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+  var commentForm = document.getElementById('comment-form');
+
+commentForm.addEventListener('submit', function(event) {
+  event.preventDefault(); // Oprirea comportamentului implicit de trimitere a formularului
+
+  var nameInput = document.getElementById('name');
+  var commentInput = document.getElementById('comment');
+
+  var name = nameInput.value;
+  var comment = commentInput.value;
+
+  // Salvarea comentariului în localStorage
+  var comments = JSON.parse(localStorage.getItem('comments')) || [];
+  comments.push({ name: name, comment: comment });
+  localStorage.setItem('comments', JSON.stringify(comments));
+
+  // Afisarea comentariului imediat în listă
+  displayComment(name, comment);
+
+  // Resetarea valorilor input-urilor
+  nameInput.value = '';
+  commentInput.value = '';
+});
+
+
+
+// Funcție pentru afișarea unui comentariu în listă
+function displayComment(name, comment) {
+  var commentList = document.getElementById('comment-list');
+
+  var commentItem = document.createElement('div');
+  commentItem.classList.add('comment-item');
+
+  var nameElement = document.createElement('h3');
+  nameElement.textContent = name;
+
+  var commentContent = document.createElement('p');
+  commentContent.textContent = comment;
+
+  var editButton = document.createElement('button');
+  editButton.textContent = 'Edit';
+  editButton.classList.add('edit-button');
+  editButton.addEventListener('click', function() {
+    // Apelul funcției de editare a comentariului
+    editComment(commentItem, name, comment);
+  });
+
+  var deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.classList.add('delete-button');
+  deleteButton.addEventListener('click', function() {
+    // Apelul funcției de ștergere a comentariului
+    deleteComment(commentItem, name, comment);
+  });
+
+  commentItem.appendChild(nameElement);
+  commentItem.appendChild(commentContent);
+  commentItem.appendChild(editButton);
+  commentItem.appendChild(deleteButton);
+
+  commentList.appendChild(commentItem);
+}
+
+// Funcție pentru editarea unui comentariu
+function editComment(commentItem, name, comment) {
+  var editedName = prompt('Introduceți noul nume:', name);
+  var editedComment = prompt('Introduceți noul comentariu:', comment);
+
+  if (editedName && editedComment) {
+    // Actualizarea informațiilor comentariului
+    var nameElement = commentItem.querySelector('h3');
+    var commentContent = commentItem.querySelector('p');
+    nameElement.textContent = editedName;
+    commentContent.textContent = editedComment;
+
+    // Actualizarea comentariului în localStorage
+    var comments = JSON.parse(localStorage.getItem('comments'));
+    var commentIndex = findCommentIndex(comments, name, comment);
+    if (commentIndex !== -1) {
+      comments[commentIndex].name = editedName;
+      comments[commentIndex].comment = editedComment;
+      localStorage.setItem('comments', JSON.stringify(comments));
+    }
+  }
+}
+
+// Funcție pentru ștergerea unui comentariu
+function deleteComment(commentItem, name, comment) {
+  if (confirm('Sunteți sigur că doriți să ștergeți acest comentariu?')) {
+    // Eliminarea comentariului din interfață
+    commentItem.remove();
+
+    // Eliminarea comentariului din localStorage
+    var comments = JSON.parse(localStorage.getItem('comments'));
+    var commentIndex = findCommentIndex(comments, name, comment);
+    if (commentIndex !== -1) {
+      comments.splice(commentIndex, 1);
+      localStorage.setItem('comments', JSON.stringify(comments));
+    }
+  }
+}
+
+// Funcție pentru găsirea indexului unui comentariu în array-ul de comentarii
+function findCommentIndex(comments, name, comment) {
+  for (var i = 0; i < comments.length; i++) {
+    if (comments[i].name === name && comments[i].comment === comment) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+var commentForm = document.getElementById("comment-form");
+commentForm.addEventListener("submit", function(event) {
+  if (!commentForm.checkValidity()) {
+    event.preventDefault(); // Prevenirea trimiterii formularului dacă nu este valid
+  }
+});
